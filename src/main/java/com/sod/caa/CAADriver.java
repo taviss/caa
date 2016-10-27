69d51c7461043685d4363d104022d1860cd0f853
tavi7395@yahoo.com <tavi7395@yahoo.com>
2016-10-27 18:35:49 +0300

    Added class for analyzing files

--- src/main/java/com/sod/caa/CAADriver.java
+++ src/main/java/com/sod/caa/CAADriver.java
@@ -2,6 +2,7 @@ package com.sod.caa;
 
 import com.sod.caa.arguments.ArgumentsList;
 import com.sod.caa.arguments.ArgumentsValidator;
+import com.sod.caa.data.FileAnalyzer;
 import com.sod.caa.data.loaders.FileLoader;
 import com.sod.caa.definitions.InstructionDefinitions;
 import com.sod.caa.definitions.loaders.InstructionDefinitionsLoader;
@@ -12,13 +13,16 @@ import org.apache.log4j.Logger;
 import java.io.File;
 
 public class CAADriver {
+    private static final Logger logger = Logger.getLogger(CAADriver.class);
+
     private InstructionDefinitions instructionDefinitions;
     private CommandLine arguments;
     private File inputFile;
-    private static final Logger logger = Logger.getLogger(CAADriver.class);
+    private FileAnalyzer fileAnalyzer;
 
     public CAADriver(String args[]) throws CAAInputException {
         this.arguments = ArgumentsValidator.validateArguments(args);
+        this.fileAnalyzer = new FileAnalyzer();
     }
 
     public static void main(String args[]) {
@@ -27,6 +31,7 @@ public class CAADriver {
             CAADriver caaDriver = new CAADriver(args);
             caaDriver.loadDefinitions();
             caaDriver.loadInputFile();
+            caaDriver.analyzeInput();
             logger.info("Execution ended successfully.");
         } catch (CAAInputException e) {
             logger.error(e.getMessage());
@@ -41,5 +46,9 @@ public class CAADriver {
     private void loadInputFile() throws CAAInputException {
         FileLoader fileLoader = new FileLoader();
         inputFile = fileLoader.loadFile(arguments);
+    }
+
+    private void analyzeInput() {
+        fileAnalyzer.analyzeFile(inputFile);
     }
 }
---
+++ src/main/java/com/sod/caa/data/FileAnalyzer.java
@@ -0,0 +1,9 @@
+package com.sod.caa.data;
+
+import java.io.File;
+
+public class FileAnalyzer {
+    public void analyzeFile(File file) {
+
+    }
+}
